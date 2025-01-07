f8572e1f56f8d1cbd5287204a8df5d47326251ae
cym1212 <william5680@naver.com>
2025-01-07 12:54:35 +0900

    [FEAT] GlobalExceptionHandler, Swagger, Jwt 설정

---
+++ src/main/java/com/mohaji/hackathon/domain/common/jwt/dto/GeneratedTokenDTO.java
@@ -0,0 +1,14 @@
+package com.mohaji.hackathon.domain.common.jwt.dto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+
+@Getter
+@Builder
+public class GeneratedTokenDTO {
+    private String accessToken;
+    private String refreshToken;
+    private int status;
+}
